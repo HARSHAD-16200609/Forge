8c7ac795ecc8ad2fab4abb3985b0939afe5efa8a
harshad diwate <harshaddiwate85@gmail.com>
2026-08-07 07:52:35 +0530

    feat : add basic types and an message handler

--- backend/src/websockets/schema/envelope.ts
+++ backend/src/websockets/schema/envelope.ts
@@ -1,5 +1,18 @@
 import z from "zod/v4";
+import { WsEvent } from "../types/events";
 
+
+export const envelopeSchema = z.object({
+    type: z.enum(WsEvent),
+    payload: z.unknown(),
+});
+
+
+export function parseEnvelope(raw: string) {
+    const json = JSON.parse(raw);
+
+    return envelopeSchema.parse(json);
+}
 
 export const messageEnvelopeSchema = z.object({
     type: z.string().min(20).max(30),
@@ -15,9 +28,10 @@ export const subscribeEnvelopeSchema = z.object({
     payload : z.object({
         content : z.string().max(500)
     })
-
+    
 })
 
 
+export type Envelope = z.infer<typeof envelopeSchema>;
 export type SubscribePayload = z.infer<typeof subscribeEnvelopeSchema>
 export type MessagePayload = z.infer<typeof messageEnvelopeSchema>
