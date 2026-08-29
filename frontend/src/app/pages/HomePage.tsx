d8be4867f4b08b49b388a57eba6393730f9704b1
harshad diwate <harshaddiwate85@gmail.com>
2026-08-30 01:27:28 +0530

    feat: add framer-motion and zustand dependencies, implement HeroSection and AnimatedGroup components, and create DotPattern component for enhanced UI animations

--- frontend/src/app/pages/HomePage.tsx
+++ frontend/src/app/pages/HomePage.tsx
@@ -1,9 +1,10 @@
+import { HeroSection } from "@/components/ui/hero-section";
 import { Outlet } from "react-router-dom";
 
 export function HomePage() {
     return (
         <div>
-            <h1>Hello world</h1>
+          <HeroSection />
             <Outlet />
         </div>
     );
